4a6abc53e17742e05aeb366f65afd3e97cd6c7f1
jetwes <j.twesmann@star-media.biz>
2018-01-24 13:15:35 +0100

    initial release

---
+++ src/Helpers/Cdn.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Kordy\Ticketit\Helpers;
+
+/**
+ * This file containes the version of different external JS and CSS libraries
+ * that are included.
+ *
+ * Class Cdn
+ */
+class Cdn
+{
+    const CodeMirror = '5.11.0';
+    const Summernote = '0.7.3';
+    const FontAwesome = '4.5.0';
+}
