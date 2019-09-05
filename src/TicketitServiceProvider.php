9d8902715b31167d10cb9e8be89a1aaf006fad5d
Jens Twesmann <j.twesmann@star-media.biz>
2019-09-05 09:17:22 +0200

    Update TicketitServiceProvider.php

--- src/TicketitServiceProvider.php
+++ src/TicketitServiceProvider.php
@@ -184,7 +184,7 @@ class TicketitServiceProvider extends ServiceProvider
         $this->app->register(\Collective\Html\HtmlServiceProvider::class);
         $this->app->register(\yajra\Datatables\DatatablesServiceProvider::class);
         $this->app->register(\Jenssegers\Date\DateServiceProvider::class);
-        $this->app->register(\Mews\Purifier\PurifierServiceProvider::class);
+        //$this->app->register(\Mews\Purifier\PurifierServiceProvider::class);
         /*
          * Create aliases for the dependency.
          */
